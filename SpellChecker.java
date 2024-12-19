4d75d3d5d2c19b8587fda3d01474c9bcfc931bff
rafaellkl11 <118642161+rafaellkl11@users.noreply.github.com>
2024-12-19 09:03:21 -0300

    Add files via upload

--- SpellChecker.java
+++ SpellChecker.java
@@ -73,12 +73,12 @@ public class SpellChecker
         return searching;
     }
     
-    public ArrayList<String> wordsStartingWith(char prefix)
+    public ArrayList<String> wordsStartingWith(String prefix)
     {
         ArrayList<String> Lista = new ArrayList<>();
         int index = 0;
         while (index < dictionary.size()){
-            if(dictionary.get(index).charAt(0) == prefix){
+            if(dictionary.get(index).startsWith(prefix)){
                 Lista.add(dictionary.get(index));
             }
             index++;
@@ -181,10 +181,11 @@ public class SpellChecker
             Arrays.sort(letrasWord);
             Arrays.sort(letrasDictionary);
             
-            new String(letrasWord);
-            new String(letrasDictionary);
+            String LetrasWord = new String(letrasWord);
+            String LetrasDictionary = new String(letrasDictionary);
             
-            if(letrasDictionary.equals(letrasWord)){
+            
+            if(LetrasDictionary.equals(LetrasWord)){
                 anagram = true;
             }
 
@@ -192,14 +193,84 @@ public class SpellChecker
                 anagrams.add(dictionary.get(index));
                 anagram = false;
             }
+            index++;
         }
         return anagrams;
     }
     
-    public ArrayList<String> difference(ArrayList<String> dictionary)
+    public ArrayList<String> difference(ArrayList<String> dictionary2)
     {
         ArrayList<String> difference = new ArrayList<>();
+        int index = 0;
+        int index2 =0;
+        searching = false;
+        while(index < dictionary.size()){
+            index2 = 0;
+            searching = false;
+            while(index2 < dictionary2.size()){
+                if(dictionary.get(index).equals(dictionary2.get(index2))){
+                    searching = true;
+                    break;
+                }
+                index2++;
+            }
+            
+            if(!searching){
+                difference.add(dictionary.get(index));
+            }            
+            index++;
+            System.out.println("test$1"+index);
+        }
+        
+        index = 0;
+        index2 = 0;
+        
+        while(index < dictionary2.size()){
+            index2 = 0;
+            while(index2 < dictionary.size()){
+                if(dictionary2.get(index).equals(dictionary.get(index2))){
+                    searching = true;
+                    break;
+                }
+                index2++;
+            }
+            
+            if(!searching){
+                difference.add(dictionary2.get(index));
+                searching = false;
+            }            
+            index++;
+            System.out.println("test&2"+index);
+        }
+        
+        index = 0; 
+        while(index < difference.size()){
+            System.out.println(difference.get(index));
+            index++;
+        }
         
         return difference;
+    }
+    
+    public int distance(String word1, String word2)
+    {
+        int index = 0;
+        int indexWord1 = 0;
+        int indexWord2 = 0;
+        int distance = 0 ;
+        while (index < dictionary.size()){
+            if (dictionary.get(index).equals(word1)){
+                indexWord1 = index;
+            }
+            if (dictionary.get(index).equals(word2)){
+                indexWord2 = index;    
+            }
+            
+        }
+        
+        distance = indexWord1 - indexWord2;
+        
+        return distance;
+        
     }
 }
